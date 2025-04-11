95a57b5961c0d75e458eb3938fbaab5b5d7c3e17
KrzysztofCwikla1 <cwikakrzysztof@gmail.com>
2025-04-12 00:34:40 +0200

    ok2

--- EShopService/Models/Category.cs
+++ EShopService/Models/Category.cs
@@ -5,6 +5,6 @@ namespace EShopService.Models
     public class Category : BaseModel
     {
         public int CategoryId { get; set; }
-        public string? Category { get; set; }
+        public string? CategoryName { get; set; }
     }
 }
